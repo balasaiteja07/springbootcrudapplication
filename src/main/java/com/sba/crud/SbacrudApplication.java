f5efa4e71bffb105e58cd51813ff7dd5eab80239
Sai Kumar <saiteja.tkr07@gmail.com>
2023-08-21 21:06:31 +0530

    initial commit

---
+++ src/main/java/com/sba/crud/SbacrudApplication.java
@@ -0,0 +1,13 @@
+package com.sba.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbacrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sba/crud/SbacrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sba.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbacrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
